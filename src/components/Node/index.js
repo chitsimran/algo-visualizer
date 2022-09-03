d8b892d76d4a2101cc828cc2c3aec1d497765c86
chitsimran <chitsimranwaraich0831@gmail.com>
2022-09-03 18:53:11 +0530

    initial dijkstra setup

---
+++ src/components/Node/index.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { NodeContainer } from "./style";
+
+const Node = ({ row, col }) => {
+    const [visited, setVisited] = useState(false);
+
+    return <NodeContainer></NodeContainer>;
+};
+
+export default Node;
